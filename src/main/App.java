e8a352087243cb00cf44da4bc7d2c6524fd1f862
ShanthakumarThanus <thanus.shanthakumar.sio@gmail.com>
2024-12-16 21:13:28 +0100

    first commit

---
+++ src/main/App.java
@@ -0,0 +1,18 @@
+package main;
+import javax.swing.*;
+public class App {
+    public static void main(String[] args) {
+        // Variable pour la fenêtre
+        int tileSize = 32;
+        int rows = 16;
+        int columns = 16;
+        int boardWidth = tileSize * columns; // = 512px
+        int boardHeight = tileSize * rows; // = 512px
+
+        JFrame frame = new JFrame("Space Invaders");
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null); // Ouverture de la fenêtre au centre de l'écran
+        frame.setResizable(false); // Taille statique
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
